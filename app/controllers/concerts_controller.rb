3951451e8a1c0aad9686ce08c92a6358a3717e1e
Grégoire <greg.joncour@gmail.com>
2016-11-20 03:34:05 +0100

    erreor message + formulaire croquis concerts

--- app/controllers/concerts_controller.rb
+++ app/controllers/concerts_controller.rb
@@ -2,6 +2,11 @@ class ConcertsController < ApplicationController
 	def index
 	end
 
+	def show
+		@concert = Concert.find(params[:id])
+	end
+
+
 	def new
 		@concert = Concert.new
 	end
--- app/models/concert.rb
+++ app/models/concert.rb
@@ -1,2 +1,4 @@
 class Concert < ApplicationRecord
+	validates :name, presence: {message: "obligatoire"}, length: { maximum: 30 }
+	validates :address, presence: {message: "obligatoire"}
 end
