5da7a606c41246371e8a085297c3e48a56eb3f80
Grégoire <greg.joncour@gmail.com>
2016-11-20 02:48:17 +0100

    genration table user et concert + generation model concerts

---
+++ db/migrate/20161120014500_create_concerts.rb
@@ -0,0 +1,13 @@
+class CreateConcerts < ActiveRecord::Migration[5.0]
+  def change
+    create_table :concerts do |t|
+      t.string :concert
+      t.integer :tickets_available
+      t.string :address
+      t.date :concert_date
+      t.string :concert_hall
+
+      t.timestamps
+    end
+  end
+end
