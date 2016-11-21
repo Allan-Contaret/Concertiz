7c2309ce450b81f7adc46f24a52b55e6a4d98b4e
Grégoire <greg.joncour@gmail.com>
2016-11-22 00:36:49 +0100

    style flash message login

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,10 +1,4 @@
 class Reservation < ApplicationRecord
-
-	belongs_to :user
-
-	validates_presence_of :name
-	validates_presence_of :firstname
-	validates_presence_of :email
-	validates_uniqueness_of :email
-	validates_presence_of :tickets_number
+  belongs_to :user
+  belongs_to :concert
 end
