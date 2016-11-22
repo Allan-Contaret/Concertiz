5574c96f973fb081558c2adf6f25d6b58cee87ad
Grégoire <greg.joncour@gmail.com>
2016-11-22 20:08:56 +0100

    tyle map

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,10 +1,41 @@
 function initMap() {
+  var customMapType = new google.maps.StyledMapType([
+    {
+      featureType: 'all',
+      stylers: [
+        { saturation: -80 }
+      ]
+    },{
+      featureType: 'road.arterial',
+      elementType: 'geometry',
+      stylers: [
+        { hue: '#00ffee' },
+        { saturation: 50 }
+      ]
+    },{
+      featureType: 'poi.business',
+      elementType: 'labels',
+      stylers: [
+        { visibility: 'off' }
+      ]
+    }
+  ], {
+      name: 'Retro'
+  });
+  var customMapTypeId = 'custom_style';
+
   var geocoder = new google.maps.Geocoder;
   var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 15,
-    center: {lat: -33.865, lng: 151.209}
+    center: {lat: -33.865, lng: 151.209},
+    mapTypeControlOptions: {
+      mapTypeIds: [google.maps.MapTypeId.ROADMAP,google.maps.MapTypeId.SATELLITE,,google.maps.MapTypeId.HYBRID, google.maps.MapTypeId.TERRAIN, customMapTypeId]
+    }
   });
   geocodeAddress(geocoder, map, address);
+  map.mapTypes.set(customMapTypeId, customMapType);
+  map.setMapTypeId(customMapTypeId);
+  
 }
 
 function geocodeAddress(geocoder, map, address) {
